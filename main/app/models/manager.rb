21e7457aed6972abbee8f22d61eb3141cae51db7
xiuce <zhaoxc@qidianbj.com>
2016-07-19 10:51:08 +0800

    manager subject checkpoints

--- main/app/models/manager.rb
+++ main/app/models/manager.rb
@@ -2,28 +2,25 @@ class Manager < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
 
-  def self.get_all_navi_menus
-    result = {"nodes" =>[]}
-    
-    # Level1: Node Structure 
-    result["nodes"] << { 
-      "id" => "000",
-      "rid" => "000",
-      "pid" => "",
-      "name" => I18n.t("managers.node_structure"),
-      "file" => ""
-    }
-
-    # Level1: Checkpoint
-    result["nodes"] << { 
-      "id" => "001",
-      "rid" => "001",
-      "pid" => "",
-      "name" => I18n.t("managers.checkpoint"),
-      "file" => "/managers/checkpoints/"
-    }
-    return result
+  def self.left_menus
+    [
+      {
+        id: 1, icon: 'icon-sys', name: '指标体系',
+        menus: [
+          {id: 12, name: '教材管理', icon: '', url: '/managers/node_structures'},
+          {id: 13, name: '指标体系管理', icon: '', url: '/managers/checkpoints'},
+          {id: 14, name: '科目指标体系管理', icon: '', url: '/managers/subject_checkpoints'}
+        ]
+      },
+      {
+        id: 2, icon: 'icon-sys', name: '用户管理',
+        menus: [
+          {id: 21, name: '角色管理', icon: '', url: '/managers/roles'},
+          {id: 22, name: '权限管理', icon: '', url: '/managers/permissions'}
+        ]
+      }
+    ]
   end
 end
