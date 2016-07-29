1542dc5db9dbf8fafe14d26a9db4063f6e61891f
sunsc <sunsc@qidianbj.com>
2016-07-29 16:34:56 +0800

    redis

--- main/config/initializers/sidekiq.rb
+++ main/config/initializers/sidekiq.rb
@@ -1,3 +1,4 @@
+=begin
 redis_server = Rails.application.secrets.redis_server
 redis_port = Rails.application.secrets.redis_port
 redis_db_num = Rails.application.secrets.redis_db_num
@@ -14,3 +15,4 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: url, namespace: redis_namespace }
 end
+=end
