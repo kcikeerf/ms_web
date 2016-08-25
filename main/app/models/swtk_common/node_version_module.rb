725d61be238e4c401e6c28673cbabc57395d28b5
sunsc <sunsc@qidianbj.com>
2016-08-25 11:14:13 +0800

    add paper basic information change

---
+++ main/app/models/swtk_common/node_version_module.rb
@@ -0,0 +1,9 @@
+module NodeVersionModule
+  module NodeVersion
+    List = {
+      :ren_jiao_ban => I18n.t("dict.ren_jiao_ban"),
+      :bei_shi_da_ban => I18n.t("dict.bei_shi_da_ban"),
+      :su_jiao_ban => I18n.t("dict.su_jiao_ban")
+    }
+  end
+end
