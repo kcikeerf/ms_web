1d429af90cdc7491c3ead945865efbec33f0e705
sunsc <sunsc@qidianbj.com>
2016-11-18 11:54:11 +0800

    update

--- main/app/models/mongodb/bank_qizpoint_qzp.rb
+++ main/app/models/mongodb/bank_qizpoint_qzp.rb
@@ -2,8 +2,6 @@
 
 class Mongodb::BankQizpointQzp
   include Mongoid::Document
-#  include Tenacity
-#  include MongoMysqlRelations
   include Mongodb::MongodbPatch
 
   before_create :set_create_time_stamp
@@ -14,9 +12,6 @@ class Mongodb::BankQizpointQzp
   validates :tbs_sid,:type, length: {maximum: 50}
   validates :answer, :desc, length: {maximum: 500}
 
-
-
-#  field :uid, type: String
   field :quz_uid, type: String
   field :pap_uid, type: String
   field :tbs_sid, type: String
@@ -29,31 +24,22 @@ class Mongodb::BankQizpointQzp
   field :dt_add, type: DateTime
   field :dt_update, type: DateTime
 
-#  has_many :bank_ckp_qzps, class_name: "Mongodb::BankCkpQzp", foreign_key: "qzp_uid"
-#  t_has_many :bank_checkpoint_ckps, through: :bank_ckp_qzps, class_name: "Mongodb::BankCkpQzp"#, foreign_key: "ckp_uid" 
-#  has_many :bank_ckp_qzps, class_name: "Mongodb::BankCkpQzp", foreign_key: "qzp_uid"
-#  to_mysql_has_many :bank_checkpoint_ckps, class_name: "BankCheckpointCkp", through: "Mongodb::BankCkpQzp"
-
   belongs_to :bank_quiz_qiz, class_name: "Mongodb::BankQuizQiz"
   has_and_belongs_to_many :bank_paper_paps, class_name: "Mongodb::BankPaperPap"
   has_many :bank_qizpoint_qzp_histories, class_name: "Mongodb::BankQizpointQzpHistory"
 
-  # 获取得分点所有指标
   #
   def bank_checkpoint_ckps
     result_arr =[]
     ckps = Mongodb::BankCkpQzp.where(qzp_uid: self._id.to_s).to_a
     ckps.each{|ckp|
       ckp_source = ckp.source_type.blank?? "BankCheckpointCkp":ckp.source_type
-      ckp_klass = ckp_source.constantize #根据指标类型取得相应指标
+      ckp_klass = ckp_source.constantize
       result_arr << ckp_klass.where(uid: ckp.ckp_uid).first
     }
     return result_arr
   end
 
-  # 结构化得分点的指标，并更新DB
-  # 用于：
-  #   1）成绩录入
   #   
   def format_ckps_json
     ckps = bank_checkpoint_ckps
@@ -68,12 +54,10 @@ class Mongodb::BankQizpointQzp
     ckps.each{|ckp|
       next unless ckp
 
-      #无科目警告
       unless ckp.subject
         logger.debug ">>>#{ckp}: no subject information<<<"
       end
 
-      #指标路径
       ckp_ancestors = BankRid.get_all_higher_nodes(ckp.families,ckp)
       ckps_arr = ckp_ancestors.push(ckp)
 
