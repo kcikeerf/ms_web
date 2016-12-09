a13067a82b2a56b456820170763406cc0a7f8cc6
sunsc <sunsc@qidianbj.com>
2016-12-09 11:46:15 +0800

    update

--- main/app/controllers/managers/checkpoints_controller.rb
+++ main/app/controllers/managers/checkpoints_controller.rb
@@ -5,11 +5,14 @@ class Managers::CheckpointsController < ApplicationController
   layout 'manager', only: [:index]
   
   def index
-
   end
 
   def combine_node_catalogs_subject_checkpoints
-    params.permit(:node_uid, :catalogs =>[], :subject_checkpoints => [])
+    params.permit!#(:node_uid, :catalogs, :checkpoints)
+
+    #p params[:catalogs].values
     
+
+    render :json => params.to_json
   end
 end
