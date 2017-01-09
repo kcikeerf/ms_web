a65e7d62ea2357783dcbb5d17c65c00b63bc873c
sunsc <sunsc@qidianbj.com>
2017-01-09 16:59:58 +0800

    logo update

--- main/db/migrate/20161018164041_remove_ana_uid_type_add_user_id_task_type_to_task_lists.rb
+++ main/db/migrate/20161018164041_remove_ana_uid_type_add_user_id_task_type_to_task_lists.rb
@@ -3,6 +3,6 @@ class RemoveAnaUidTypeAddUserIdTaskTypeToTaskLists < ActiveRecord::Migration
   	remove_column :task_lists, :ana_uid, :string
   	#remove_column :task_lists, :type, :string
   	add_column :task_lists, :user_id, :string
-  	#add_column :task_lists, :task_type, :string
+  	add_column :task_lists, :task_type, :string
   end
 end
