6790c5500312da9a77c75705592a64b6e226c0bd
sunsc <sunsc@qidianbj.com>
2017-01-10 23:56:58 +0800

    update

--- main/app/controllers/errors_controller.rb
+++ main/app/controllers/errors_controller.rb
@@ -1,9 +1,9 @@
 class ErrorsController < ApplicationController
   def error_404
-    render :status => 404, :formats =>[:html], :layout => "errors"
+    render :status => 404, :formats =>[:html], :layout => "error"
   end
 
   def error_500
-    render :status => 500, :formats =>[:html], :layout => "errors"
+    render :status => 500, :formats =>[:html], :layout => "error"
   end
 end
