29d52983c4eb5307b28c1008c968f3dd26979f44
sunsc <sunsc@qidianbj.com>
2017-01-13 12:49:49 +0800

    update

--- main/app/api/wx/reports_warehouse.rb
+++ main/app/api/wx/reports_warehouse.rb
@@ -22,7 +22,7 @@ module ReportsWarehouse
       end
       post '*any_path' do
         target_file_path = request.fullpath.to_s.split("/api/wx/v1.1")[1]
-
+        target_user = current_user
         if !params[:any_path].blank? && File.exist?(target_file_path)
           path_arr = params[:any_path].split("/")
           current_group = nil
@@ -47,67 +47,67 @@ module ReportsWarehouse
 
             Common::Report::Group::ListArr.map{|item| path_h[item] = {:value => nil, :allowed_file_regx => [generic_re]} }
             # 学生
-            if current_user.is_pupil?
+            if target_user.is_pupil?
               # current_group = Common::Report::Group::Pupil
-              # group_ids = [current_user.pupil.uid]
+              # group_ids = [target_user.pupil.uid]
 
               start_index = Common::Report::Group::ListArr.find_index(Common::Report::Group::Pupil)
               
-              path_h[Common::Report::Group::Pupil][:value] = [current_user.role_obj.uid]
-              path_h[Common::Report::Group::Klass][:value] = [current_user.role_obj.location.uid]
-              path_h[Common::Report::Group::Grade][:value] = [current_user.tenant.uid]
-              target_papers = current_user.role_obj.papers
+              path_h[Common::Report::Group::Pupil][:value] = [target_user.role_obj.uid]
+              path_h[Common::Report::Group::Klass][:value] = [target_user.role_obj.location.uid]
+              path_h[Common::Report::Group::Grade][:value] = [target_user.tenant.uid]
+              target_papers = target_user.role_obj.papers
               path_h[Common::Report::Group::Project][:allowed_file_regx] += [paper_info_re]
 
             # 教师
-            elsif current_user.is_teacher?
+            elsif target_user.is_teacher?
               # current_group = Common::Report::Group::Grade
-              # group_ids = [current_user.tenant.uid]
+              # group_ids = [target_user.tenant.uid]
 
               start_index = Common::Report::Group::ListArr.find_index(Common::Report::Group::Pupil)
 
-              path_h[Common::Report::Group::Pupil][:value] = current_user.role_obj.pupils.map{|item| item.uid}
-              path_h[Common::Report::Group::Klass][:value] = current_user.role_obj.locations.map{|item| item.uid}
+              path_h[Common::Report::Group::Pupil][:value] = target_user.role_obj.pupils.map{|item| item.uid}
+              path_h[Common::Report::Group::Klass][:value] = target_user.role_obj.locations.map{|item| item.uid}
               path_h[Common::Report::Group::Klass][:allowed_file_regx] += [nav_re]
-              path_h[Common::Report::Group::Grade][:value] = [current_user.tenant.uid]
-              target_papers = current_user.role_obj.papers
+              path_h[Common::Report::Group::Grade][:value] = [target_user.tenant.uid]
+              target_papers = target_user.role_obj.papers
               path_h[Common::Report::Group::Project][:allowed_file_regx] += [paper_info_re]
 
             # 分析员
-            elsif current_user.is_analyzer?
+            elsif target_user.is_analyzer?
               # current_group = Common::Report::Group::Grade
-              # group_ids = [current_user.tenant.uid]
+              # group_ids = [target_user.tenant.uid]
 
               start_index = Common::Report::Group::ListArr.find_index(Common::Report::Group::Grade)
 
               path_h[Common::Report::Group::Klass][:allowed_file_regx] += [nav_re] 
-              path_h[Common::Report::Group::Grade][:value] = [current_user.tenant.uid]
+              path_h[Common::Report::Group::Grade][:value] = [target_user.tenant.uid]
               path_h[Common::Report::Group::Grade][:allowed_file_regx] += [nav_re] 
-              target_papers = current_user.role_obj.papers
+              target_papers = target_user.role_obj.papers
               path_h[Common::Report::Group::Project][:allowed_file_regx] += [paper_info_re]
 
             # 租户管理员
-            elsif current_user.is_tenant_administrator?
+            elsif target_user.is_tenant_administrator?
               # current_group = Common::Report::Group::Grade
-              # group_ids = [current_user.tenant.uid]
+              # group_ids = [target_user.tenant.uid]
 
               start_index = Common::Report::Group::ListArr.find_index(Common::Report::Group::Grade)
 
               path_h[Common::Report::Group::Klass][:allowed_file_regx] += [nav_re]
-              path_h[Common::Report::Group::Grade][:value] = [current_user.tenant.uid]
+              path_h[Common::Report::Group::Grade][:value] = [target_user.tenant.uid]
               path_h[Common::Report::Group::Grade][:allowed_file_regx] += [nav_re] 
-              target_papers = current_user.tenant.papers
+              target_papers = target_user.tenant.papers
               path_h[Common::Report::Group::Project][:allowed_file_regx] += [paper_info_re] 
             # 项目管理员
-            elsif current_user.is_project_administrator?
+            elsif target_user.is_project_administrator?
               # current_group = Common::Report::Group::Project
-              # group_ids = current_user.bank_tests.map{|item| item.id.to_s }
+              # group_ids = target_user.bank_tests.map{|item| item.id.to_s }
 
               start_index = Common::Report::Group::ListArr.find_index(Common::Report::Group::Project)
 
               path_h[Common::Report::Group::Klass][:allowed_file_regx] += [nav_re]
               path_h[Common::Report::Group::Grade][:allowed_file_regx] += [nav_re]
-              target_papers = current_user.role_obj.papers
+              target_papers = target_user.role_obj.papers
               path_h[Common::Report::Group::Project][:allowed_file_regx] += [paper_info_re, nav_re] 
             end
           end
