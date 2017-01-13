1818cf6e1c98ae39530cad2f078faeae93b8a6ee
sunsc <sunsc@qidianbj.com>
2017-01-13 12:28:49 +0800

    update

---
+++ main/db/migrate/20170113042230_add_indexes_to_v11_tables.rb
@@ -0,0 +1,17 @@
+class AddIndexesToV11Tables < ActiveRecord::Migration
+  def change
+  	add_index :users, :part_number
+  	add_index :pupils, :user_id
+  	add_index :pupils, :loc_uid
+  	add_index :pupils, :stu_number
+  	add_index :pupils, :tenant_uid
+  	add_index :teachers, :user_id
+  	add_index :teachers, :loc_uid
+  	add_index :tachers, :tenant_uid
+  	add_index :wx_users, :wx_openid
+  	add_index :locations, :tenant_uid
+  	add_index :class_teacher_mapping, :tea_uid
+  	add_index :class_teacher_mapping, :loc_uid
+  	add_index :class_teacher_mapping, :tenant_uid
+  end
+end
