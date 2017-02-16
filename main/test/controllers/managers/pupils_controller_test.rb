04860919200d61cc821908a205db136ee44a50bc
sunsc <sunsc@qidianbj.com>
2017-02-16 13:07:28 +0800

    remove managers

--- main/test/controllers/managers/pupils_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Managers::PupilsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- main/test/controllers/managers/teachers_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Managers::TeachersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
