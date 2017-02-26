5aee5bcfa8a125907d2e614780bf85d8d45f54ac
sunsc <sunsc@qidianbj.com>
2017-02-26 20:21:33 +0800

    update

--- main/app/models/swtk_common/klass_module.rb
+++ main/app/models/swtk_common/klass_module.rb
@@ -1,70 +1,114 @@
 module KlassModule
   module Klass
     module_function
-    List = {
-      :yi_ban => Common::Locale::i18n("dict.yi_ban"),
-      :er_ban => Common::Locale::i18n("dict.er_ban"),
-      :san_ban => Common::Locale::i18n("dict.san_ban"),
-      :si_ban => Common::Locale::i18n("dict.si_ban"),
-      :wu_ban => Common::Locale::i18n("dict.wu_ban"),
-      :liu_ban => Common::Locale::i18n("dict.liu_ban"),
-      :qi_ban => Common::Locale::i18n("dict.qi_ban"),
-      :ba_ban => Common::Locale::i18n("dict.ba_ban"),
-      :jiu_ban => Common::Locale::i18n("dict.jiu_ban"),
-      :shi_ban => Common::Locale::i18n("dict.shi_ban"),
-      :shi_yi_ban => Common::Locale::i18n("dict.shi_yi_ban"),
-      :shi_er_ban => Common::Locale::i18n("dict.shi_er_ban"),
-      :shi_san_ban => Common::Locale::i18n("dict.shi_san_ban"),
-      :shi_si_ban => Common::Locale::i18n("dict.shi_si_ban"),
-      :shi_wu_ban => Common::Locale::i18n("dict.shi_wu_ban"),
-      :shi_liu_ban => Common::Locale::i18n("dict.shi_liu_ban"),
-      :shi_qi_ban => Common::Locale::i18n("dict.shi_qi_ban"),
-      :shi_ba_ban => Common::Locale::i18n("dict.shi_ba_ban"),
-      :shi_jiu_ban => Common::Locale::i18n("dict.shi_jiu_ban"),
-      :er_shi_ban => Common::Locale::i18n("dict.er_shi_ban"),
-      :er_shi_yi_ban => Common::Locale::i18n("dict.er_shi_yi_ban"),
-      :er_shi_er_ban => Common::Locale::i18n("dict.er_shi_er_ban"),
-      :er_shi_san_ban => Common::Locale::i18n("dict.er_shi_san_ban"),
-      :er_shi_si_ban => Common::Locale::i18n("dict.er_shi_si_ban"),
-      :er_shi_wu_ban => Common::Locale::i18n("dict.er_shi_wu_ban"),
-      :er_shi_liu_ban => Common::Locale::i18n("dict.er_shi_liu_ban"),
-      :er_shi_qi_ban => Common::Locale::i18n("dict.er_shi_qi_ban"),
-      :er_shi_ba_ban => Common::Locale::i18n("dict.er_shi_ba_ban"),
-      :er_shi_jiu_ban => Common::Locale::i18n("dict.er_shi_jiu_ban"),
-      :san_shi_ban => Common::Locale::i18n("dict.san_shi_ban")
+    KlassArr = %W{
+      yi_ban
+      er_ban
+      san_ban 
+      si_ban
+      wu_ban
+      liu_ban 
+      qi_ban
+      ba_ban
+      jiu_ban 
+      shi_ban 
+      shi_yi_ban
+      shi_er_ban
+      shi_san_ban 
+      shi_si_ban
+      shi_wu_ban
+      shi_liu_ban 
+      shi_qi_ban
+      shi_ba_ban
+      shi_jiu_ban 
+      er_shi_ban
+      er_shi_yi_ban
+      er_shi_er_ban
+      er_shi_san_ban 
+      er_shi_si_ban
+      er_shi_wu_ban
+      er_shi_liu_ban 
+      er_shi_qi_ban
+      er_shi_ba_ban
+      er_shi_jiu_ban 
+      san_shi_ban
+      san_shi_yi_ban
+      san_shi_er_ban
+      san_shi_san_ban
+      san_shi_si_ban
+      san_shi_wu_ban
+      san_shi_liu_ban
+      san_shi_qi_ban
+      san_shi_ba_ban
+      san_shi_jiu_ban
+      si_shi_ban
+      si_shi_yi_ban
+      si_shi_er_ban
+      si_shi_san_ban
+      si_shi_si_ban
+      si_shi_wu_ban
+      si_shi_liu_ban
+      si_shi_qi_ban
+      si_shi_ba_ban
+      si_shi_jiu_ban
+      wu_shi_ban
+      wu_shi_yi_ban
+      wu_shi_er_ban
+      wu_shi_san_ban
+      wu_shi_si_ban 
+      wu_shi_wu_ban
+      wu_shi_liu_ban
+      wu_shi_qi_ban
+      wu_shi_ba_ban
+      wu_shi_jiu_ban
+      liu_shi_ban
+      liu_shi_yi_ban
+      liu_shi_er_ban
+      liu_shi_san_ban
+      liu_shi_si_ban
+      liu_shi_wu_ban
+      liu_shi_liu_ban
+      liu_shi_qi_ban
+      liu_shi_ba_ban
+      liu_shi_jiu_ban
+      qi_shi_ban
+      qi_shi_yi_ban
+      qi_shi_er_ban
+      qi_shi_san_ban
+      qi_shi_si_ban
+      qi_shi_wu_ban
+      qi_shi_liu_ban
+      qi_shi_qi_ban
+      qi_shi_ba_ban
+      qi_shi_jiu_ban
+      ba_shi_ban
+      ba_shi_yi_ban
+      ba_shi_er_ban
+      ba_shi_san_ban
+      ba_shi_si_ban
+      ba_shi_wu_ban
+      ba_shi_liu_ban
+      ba_shi_qi_ban
+      ba_shi_ba_ban 
+      ba_shi_jiu_ban
+      jiu_shi_ban
+      jiu_shi_yi_ban
+      jiu_shi_er_ban
+      jiu_shi_san_ban
+      jiu_shi_si_ban
+      jiu_shi_wu_ban
+      jiu_shi_liu_ban
+      jiu_shi_qi_ban
+      jiu_shi_ba_ban
+      jiu_shi_jiu_ba
+      yi_bai_ban 
     }
 
-    Order ={
-      "yi_ban" => "1",
-      "er_ban" => "2",
-      "san_ban" => "3",
-      "si_ban" => "4",
-      "wu_ban" => "5",
-      "liu_ban" => "6",
-      "qi_ban" => "7",
-      "ba_ban" => "8",
-      "jiu_ban" => "9",
-      "shi_ban" => "10",
-      "shi_yi_ban" => "11",
-      "shi_er_ban" => "12",
-      "shi_san_ban" => "13",
-      "shi_si_ban" => "14",
-      "shi_wu_ban" => "15",
-      "shi_liu_ban" => "16",
-      "shi_qi_ban" => "17",
-      "shi_ba_ban" => "18",
-      "shi_jiu_ban" => "19",
-      "er_shi_ban" => "20",
-      "er_shi_yi_ban" => "21",
-      "er_shi_er_ban" => "22",
-      "er_shi_san_ban" => "23",
-      "er_shi_si_ban" => "24",
-      "er_shi_wu_ban" => "25",
-      "er_shi_liu_ban" => "26",
-      "er_shi_qi_ban" => "27",
-      "er_shi_ba_ban" => "28",
-      "er_shi_jiu_ban" => "29",
-      "san_shi_ban" => "30"
+    List = {}
+    Order = {}
+    KlassArr.each_with_index{|item, index|
+      List[item.to_sym] = Common::Locale::i18n("dict.#{item}")
+      Order[item] = (index + 1).to_s
     }
 
     def klass_label klassroom
