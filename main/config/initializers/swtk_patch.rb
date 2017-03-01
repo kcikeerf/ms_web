02a984f28e96aed2a450d4924c6cf1493382030d
sunsc <sunsc@qidianbj.com>
2017-03-02 00:21:12 +0800

    update

--- main/config/initializers/swtk_patch.rb
+++ main/config/initializers/swtk_patch.rb
@@ -48,73 +48,43 @@ class String
   end
 end
 
-module ReportPlusModule
-  module ReportPlus
-    module_function
+module TkEncryption
+  module_function
+  def codes_str_decryption code_file, secret_file
     begin
-      core_file = Rails.root + "config/initializers/report_plus_patch.txt"
-      encrypted_core_str = File.open(core_file, 'rb').read
-      secret_file = Rails.root + "tmp/tk_secret.txt"
+      encrypted_core_str = File.open(code_file, 'rb').read
       secrect_codes = nil
       if File.exists?(secret_file)
         secrect_codes = File.open(secret_file, 'rb').read
         secrect_codes.strip!
         secrect_codes.chomp!
       end
-      eval(secrect_codes.blank?? encrypted_core_str.decrypt : encrypted_core_str.decrypt(secrect_codes))
+      return (secrect_codes.blank?? encrypted_core_str.decrypt : encrypted_core_str.decrypt(secrect_codes))
     rescue Exception => ex
-      
+      puts "initialization error!"
     end
   end
 end
 
-class Mongodb::ReportConstructor
-  begin
-    core_file = Rails.root + "config/initializers/report_constructor_patch.txt"
-    encrypted_core_str = File.open(core_file, 'rb').read
+module ReportPlusModule
+  module ReportPlus
+    module_function
+    code_file = Rails.root + "config/initializers/report_plus_patch.txt"
     secret_file = Rails.root + "tmp/tk_secret.txt"
-    secrect_codes = nil
-    if File.exists?(secret_file)
-      secrect_codes = File.open(secret_file, 'rb').read
-      secrect_codes.strip!
-      secrect_codes.chomp!
-    end
-    class_eval(secrect_codes.blank?? encrypted_core_str.decrypt : encrypted_core_str.decrypt(secrect_codes))
-  rescue Exception => ex
-    #
-  end  
+    eval(TkEncryption::codes_str_decryption(code_file, secret_file))
+  end
 end
 
-class Mongodb::ReportGroupGenerator
-  begin
-    core_file = Rails.root + "config/initializers/report_group_generator_patch.txt"
-    encrypted_core_str = File.open(core_file, 'rb').read
-    secret_file = Rails.root + "tmp/tk_secret.txt"
-    secrect_codes = nil
-    if File.exists?(secret_file)
-      secrect_codes = File.open(secret_file, 'rb').read
-      secrect_codes.strip!
-      secrect_codes.chomp!
-    end
-    class_eval(secrect_codes.blank?? encrypted_core_str.decrypt : encrypted_core_str.decrypt(secrect_codes))
-  rescue Exception => ex
-    #
-  end  
-end
-
-class Mongodb::ReportPupilGenerator
-  begin
-    core_file = Rails.root + "config/initializers/report_pupil_generator_patch.txt"
-    encrypted_core_str = File.open(core_file, 'rb').read
-    secret_file = Rails.root + "tmp/tk_secret.txt"
-    secrect_codes = nil
-    if File.exists?(secret_file)
-      secrect_codes = File.open(secret_file, 'rb').read
-      secrect_codes.strip!
-      secrect_codes.chomp!
-    end
-    class_eval(secrect_codes.blank?? encrypted_core_str.decrypt : encrypted_core_str.decrypt(secrect_codes))
-  rescue Exception => ex
-    #
-  end  
+target_klass_arr = %W{
+  report_constructor
+  report_pupil_generator
+  report_group_generator
+}
+target_klass_arr.each do |klass|
+  core_file = Rails.root + "config/initializers/#{klass}_patch.txt"
+  secret_file = Rails.root + "tmp/tk_secret.txt"
+  puts TkEncryption::codes_str_decryption(core_file, secret_file) if klass == "report_pupil_generator"
+  "Mongodb::#{klass.camelize}".constantize.class_eval do
+    eval(TkEncryption::codes_str_decryption(core_file, secret_file))
+  end
 end
