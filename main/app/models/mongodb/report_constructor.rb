5b2fdef321a83137f754acd556e10e2894f7da2d
sunsc <sunsc@qidianbj.com>
2017-03-06 17:51:19 +0800

    bug ifx

--- main/app/models/mongodb/report_constructor.rb
+++ main/app/models/mongodb/report_constructor.rb
@@ -5,10 +5,10 @@ require 'thwait'
 
 class Mongodb::ReportConstructor
   include Mongoid::Document
-  code_file = Rails.root + "config/initializers/patches/mongodb_report_constructor.txt"
+  code_file = Rails.root + "config/initializers/patches/mongodb_report_constructor_patch.txt"
   begin
     eval(TkEncryption::codes_str_decryption(code_file))
   rescue Exception => ex
     #
-  end     
+  end
 end
