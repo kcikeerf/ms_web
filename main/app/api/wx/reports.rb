b01014c3967c02df3cdd9941deea03c682651fac
sunsc <sunsc@qidianbj.com>
2017-03-07 13:55:35 +0800

    api update

--- main/app/api/wx/reports.rb
+++ main/app/api/wx/reports.rb
@@ -9,6 +9,48 @@ module Reports
     helpers ApiHelper
     helpers SharedParamsHelper
 
+    helpers do
+      def read_report_data report_path
+        result = {}
+        if Common::SwtkRedis::has_key? Common::SwtkRedis::Ns::Cache, report_path
+          puts "true"
+          target_report_j = Common::SwtkRedis::get_value Common::SwtkRedis::Ns::Cache, report_path
+          result = JSON.parse(target_report_j)
+        else
+          puts "false"
+          target_report_f = Dir[report_path].first
+          return result if target_report_f.blank?
+          target_report_data = File.open(target_report_f, 'rb').read
+          return result if target_report_data.blank?
+          result = JSON.parse(target_report_data)
+          Common::SwtkRedis::set_key Common::SwtkRedis::Ns::Cache, report_path, result.to_json
+        end
+        return result
+      end
+
+      def construct_report_content current_group, report_url
+        result = {}
+        report_url.sub!(".json","")
+        url_arr = report_url.split("/")
+        return result if url_arr.blank? || !url_arr.include?("tests")
+
+        group_start_index = Common::Report::Group::ListArr.find_index{|item| item == current_group }
+        group_arr = Common::Report::Group::ListArr[group_start_index..-1]
+        # 获取试卷信息
+        tests_index = url_arr.find_index{|item| item == "tests"}
+        paper_info_path = Common::Report::WareHouse::ReportLocation + url_arr[0..(tests_index + 1)].join("/") + "/paper_info.json"        
+        result["paper_info"] = read_report_data(paper_info_path)
+
+        # 获取个分组报告信息
+        group_arr.each{|group|
+          group_index = url_arr.find_index{|item| item == group}
+          target_report_path = Common::Report::WareHouse::ReportLocation + url_arr[0..(group_index+1)].join("/") + ".json"
+          result[group] = read_report_data(target_report_path)
+        }
+        return result
+      end
+    end
+
     resource :reports do
       before do
         set_api_header
@@ -131,16 +173,15 @@ module Reports
 
       ###########
 
-      desc '获取当前用户所在租户的年级班级列表 post /api/wx/v1.1/reports/klass_list' # grade_class_list begin
+      desc '获取当前用户所在租户的年级班级列表 post /api/wx/v1.1/reports/klass_list' # class_list begin
       params do
         use :authenticate
-        requires :test_id, type: String, allow_blank: true
-        optional :tenant_uid, type: String, allow_blank: true
+        requires :test_id, type: String, allow_blank: false
+        optional :tenant_uid, type: String, allow_blank: false
       end
       post :klass_list do
         result = []
 
-        
         if params[:tenant_uid].blank?
           accessable_tenant_uids = current_user.accessable_tenants.map(&:uid)
           default_tenant_uid = accessable_tenant_uids[0]
@@ -149,21 +190,60 @@ module Reports
         end
         accessable_loc_uids = current_user.accessable_locations.map(&:uid)
   
-
         nav_h = {}
-        Find.find("/reports_warehouse/tests/#{params[:test_id]}"){|f|
-          if f =~ /.*#{default_tenant_uid}\/nav.json/
-            data = File.open(f, 'rb').read
-            nav_h = JSON.parse(data)
-            break
-          end
-        }
+        nav_f = Dir[Common::Report::WareHouse::ReportLocation + "reports_warehouse/tests/" + params[:test_id]+ '/**/grade/' + params[:tenant_uid] + '/nav.json'].first
+        nav_data = File.open(nav_f, 'rb').read if !nav_f.blank?
+        nav_h = JSON.parse(nav_data) if !nav_data.blank?
+        result = nav_h.values[0].map{|item| item if accessable_loc_uids.include?(item[1]["uid"])}.compact if !nav_h.values.blank?
 
-        nav_h.values[0].map{|item| item if accessable_loc_uids.include?(item[1]["uid"])}.compact
+        result
+      end # class_list end
 
-      end # grade_class_list end
+      ###########
 
-      ###########      
+      desc '获取学生报告 post /api/wx/v1.1/reports/pupil' # pupil report begin
+      params do
+        use :authenticate
+        requires :report_url, type: String, allow_blank: false
+      end
+      post :pupil do
+        construct_report_content(Common::Report::Group::Pupil, params[:report_url])
+      end # pupil report end
+
+      ###########         
+
+      desc '获取班级报告 post /api/wx/v1.1/reports/klass' # klass report begin
+      params do
+        use :authenticate
+        requires :report_url, type: String, allow_blank: false
+      end
+      post :klass do
+        construct_report_content(Common::Report::Group::Klass, params[:report_url])
+      end # klass report end
+
+      ###########
+
+      desc '获取年级报告 post /api/wx/v1.1/reports/grade' # grade report begin
+      params do
+        use :authenticate
+        requires :report_url, type: String, allow_blank: false
+      end
+      post :grade do
+        construct_report_content(Common::Report::Group::Grade, params[:report_url])
+      end # grade report end
+
+      ###########
+
+      desc '获取区域报告 post /api/wx/v1.1/reports/project' # project report begin
+      params do
+        use :authenticate
+        requires :report_url, type: String, allow_blank: false
+      end
+      post :project do
+        construct_report_content(Common::Report::Group::Project, params[:report_url])
+      end # project report end
+
+      ###########
 
     end
 
