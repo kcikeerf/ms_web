07b7be4435ba9cfca8c1934c9bc6207fff934e4c
sunsc <sunsc@qidianbj.com>
2017-06-23 16:50:05 +0800

    update

--- main/app/models/tk_lock.rb
+++
@@ -1,5 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-class TkLock < ActiveRecord::Base
-  belongs_to :lock_resource, polymorphic: true
-end
--- main/test/models/tk_lock_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class TkLockTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
