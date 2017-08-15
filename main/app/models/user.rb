bc5593b6add75c7c7c2c2183f6420a05f2b1fae7
sunsc <sunsc@qidianbj.com>
2017-08-15 15:24:25 +0800

    bug fix

--- main/app/models/user.rb
+++ main/app/models/user.rb
@@ -13,9 +13,9 @@ class User < ActiveRecord::Base
   has_many :wx_user_mappings, foreign_key: "user_id", dependent: :destroy
   has_many :wx_users, through: :wx_user_mappings
   has_many :task_lists, foreign_key: "user_id"
-  has_many :oauth_access_tokens, foreign_key: "resource_owner_id", class_name: "Doorkeeper::AccessToken", dependent: :destroy
-  has_many :oauth_access_grants, foreign_key: "resource_owner_id", class_name: "Doorkeeper::AccessGrant", dependent: :destroy
-  has_many :oauth_applications, foreign_key: "resource_owner_id", class_name: "Doorkeeper::Application", dependent: :destroy
+  has_many :oauth_access_tokens, foreign_key: "resource_owner_id", class_name: "Doorkeeper::AccessToken"#, dependent: :destroy
+  has_many :oauth_access_grants, foreign_key: "resource_owner_id", class_name: "Doorkeeper::AccessGrant"#, dependent: :destroy
+  has_many :oauth_applications, foreign_key: "resource_owner_id", class_name: "Doorkeeper::Application"#, dependent: :destroy
   has_many :groups_as_parent, :foreign_key=>"child_id", :class_name=>'UserLink'
   has_many :groups_as_child, :foreign_key => "parent_id", :class_name=>"UserLink"
   has_many :parents, :through=>:groups_as_parent
