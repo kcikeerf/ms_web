908d03d3d38ad8a4909e875e0e41ed895a123f2b
clover <759983462@qq.com>
2017-09-21 15:33:33 +0800

    获取指标包含的试题

---
+++ main/app/api/v12/api_v12_tests.rb
@@ -0,0 +1,45 @@
+# -*- coding: UTF-8 -*-
+
+module ApiV12Tests
+  class API < Grape::API
+    format :json
+
+    helpers Doorkeeper::Grape::Helpers
+    helpers ApiV12Helper
+    helpers ApiV12SharedParamsHelper
+
+    params do
+      use :oauth
+    end
+
+    resource :tests do #checkpoints begin
+      before do
+        set_api_header
+        doorkeeper_authorize!
+        authenticate_api_permission current_user.id, request.request_method, request.fullpath
+      end
+
+
+      desc '"试卷指标题mapping" post /api/v1.2/tests/paper_ckps_qzps_mapping' # submit_analyze begin
+      params do
+        requires :test_uid, type: String
+      end
+      post :paper_ckps_qzps_mapping do
+        # authenticate_api_permission current_user.id, "POST" , "/api/v1.2/users/get_binded_users"
+        bank_test = Mongodb::BankTest.where(id: params[:test_uid]).first
+        if bank_test 
+          paper = bank_test.bank_paper_pap
+          if paper
+            paper.associated_checkpoints
+          else
+            error!(message_json("e40405"), 404)
+          end 
+        else
+          error!(message_json("e40405"), 404)
+        end
+      end
+
+
+    end
+  end
+end
